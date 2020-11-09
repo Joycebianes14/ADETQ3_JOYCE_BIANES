69a93ecb73606648bfc957a91d701dbdb2df0e8f
Joyce Bianes <1013-18@itmlyceumalabang.onmicrosoft.com>
2020-11-10 00:38:00 +0800

    Joyce Bianes

---
+++ ADETQ3_JOYCE_BIANES/ADETQ3_JOYCE_BIANES/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_JOYCE_BIANES.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
